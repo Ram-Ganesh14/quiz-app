2aef4b6ae2b62f982a6d66a65e09b3dc2158d925
Ram Ganesh E <ramganeshelanselvam@gmail.com>
2024-11-03 00:55:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; // Import Tailwind's CSS
 import App from './components/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <App />
